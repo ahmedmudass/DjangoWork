3efa4e4464aa61f38412de445348ddf1b2d3116d
ahmedmudass <131138880+ahmedmudass@users.noreply.github.com>
2025-08-19 13:12:12 +0500

    adminpanel

--- mera_project/myapp/models.py
+++ mera_project/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    salary = models.CharField(max_length=55)
+    designation = models.CharField(max_length=60)
+    department = models.CharField(max_length=70)
+    qualification = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+
+
